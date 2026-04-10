8122e20f9f8560e99c451f9e68b24b6a3bca582e
Dimitrios Simatos <dsimat@yahoo.com>
2026-04-10 13:29:52 -0400

    Added docstrings

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -7,11 +7,13 @@ from analysis import analyze_data_hourly, analyze_data_daily
 
 @pytest.fixture
 def filepath():
+    """Fixture to provide the file path for the test data."""
     yield Path(__file__).parent
 
 
 @pytest.fixture
 def sample_hourly_metric_data(filepath):
+    """Fixture to load the sample hourly metric data from a JSON file and yield it as a DataFrame."""
     hourly_metric_data = json.load(
         open(f"{filepath}/mock_data/tabular_hourly_metric.json")
     )
@@ -20,6 +22,7 @@ def sample_hourly_metric_data(filepath):
 
 @pytest.fixture
 def sample_hourly_imperial_data(filepath):
+    """Fixture to load the sample hourly imperial data from a JSON file and yield it as a DataFrame."""
     hourly_imperial_data = json.load(
         open(f"{filepath}/mock_data/tabular_hourly_imperial.json")
     )
@@ -28,6 +31,7 @@ def sample_hourly_imperial_data(filepath):
 
 @pytest.fixture
 def sample_daily_metric_data(filepath):
+    """Fixture to load the sample daily metric data from a JSON file and yield it as a DataFrame."""
     daily_metric_data = json.load(
         open(f"{filepath}/mock_data/tabular_daily_metric.json")
     )
@@ -36,6 +40,7 @@ def sample_daily_metric_data(filepath):
 
 @pytest.fixture
 def sample_daily_imperial_data(filepath):
+    """Fixture to load the sample daily imperial data from a JSON file and yield it as a DataFrame."""
     daily_imperial_data = json.load(
         open(f"{filepath}/mock_data/tabular_daily_imperial.json")
     )
@@ -44,15 +49,18 @@ def sample_daily_imperial_data(filepath):
 
 @pytest.fixture
 def analyzed_hourly_data(sample_hourly_metric_data):
+    """Fixture to analyze the sample hourly metric data and yield the results."""
     yield analyze_data_hourly(sample_hourly_metric_data)
 
 
 @pytest.fixture
 def analyzed_daily_data(sample_daily_metric_data):
+    """Fixture to analyze the sample daily metric data and yield the results."""
     yield analyze_data_daily(sample_daily_metric_data)
 
 
 def test_instance_of_dataFrames(analyzed_hourly_data, analyzed_daily_data):
+    """Test that the outputs of the analysis functions are DataFrames."""
 
     for index, df in enumerate(analyzed_hourly_data):
         assert isinstance(df, pd.DataFrame), (
@@ -66,6 +74,7 @@ def test_instance_of_dataFrames(analyzed_hourly_data, analyzed_daily_data):
 
 
 def test_non_empty_dataFrames(analyzed_hourly_data, analyzed_daily_data):
+    """Test that the DataFrames returned by the analysis functions are not empty."""
 
     for df in analyzed_hourly_data:
         assert not df.empty
